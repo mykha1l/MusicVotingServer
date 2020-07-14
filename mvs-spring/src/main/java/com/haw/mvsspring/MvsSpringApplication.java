7aefe98f038113a1f7d3ababa4c42ed16ee15a07
Mykhailo Svyrydovych <mykhailo@froglogic.com>
2020-07-14 12:24:04 +0200

    Create sub-projects folders
    MVS-WebApp - for browser app, mvs_flutter - for flutter app
    and mvs-spring - for Java Spring

---
+++ mvs-spring/src/main/java/com/haw/mvsspring/MvsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.haw.mvsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvsSpringApplication.class, args);
+	}
+
+}
---
+++ mvs-spring/src/test/java/com/haw/mvsspring/MvsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haw.mvsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
