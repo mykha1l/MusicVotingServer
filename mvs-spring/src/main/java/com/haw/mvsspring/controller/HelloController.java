89704e90b54f4396a4c1983158b37a11be102650
hazletgathigia <hazletgathigia@gmail.com>
2020-07-21 20:44:15 +0200

    Adder Class body now present Methods in HelloController now included

--- mvs-spring/src/main/java/com/haw/mvsspring/controller/HelloController.java
+++ mvs-spring/src/main/java/com/haw/mvsspring/controller/HelloController.java
@@ -1,14 +1,34 @@
 package com.haw.mvsspring.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.haw.mvsspring.model.Adder;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
 
+@RequestMapping
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	@GetMapping("/")
+	public String index()
+	{
+		return "WebApp says hello!";
 	}
 
+	@GetMapping("/sum")
+	public int getSum(@RequestBody Adder myAdder)
+	{
+		int a = myAdder.getFirstInt();
+		int b = myAdder.getSecondInt();
+
+		return a + b;
+	}
+
+	@GetMapping("/subtraction")
+	public int getDifference(@RequestBody HashMap<String, Integer> myMap)
+	{
+		int a = myMap.get("firstInt");
+		int b = myMap.get("secondInt");
+
+		return a - b;
+	}
 }
--- mvs-spring/src/main/java/com/haw/mvsspring/model/Adder.java
+++ mvs-spring/src/main/java/com/haw/mvsspring/model/Adder.java
@@ -1,4 +1,26 @@
 package com.haw.mvsspring.model;
 
+//Adder class to use as a java object for the testing request mapping with two integers
 public class Adder {
+    private int firstInt;
+    private int secondInt;
+
+    public void setFirstInt(Integer firstInt)
+    {
+        this.firstInt = firstInt;
+    }
+    public void setSecondInt(Integer secondInt)
+    {
+        this.secondInt = secondInt;
+    }
+
+    public Integer getFirstInt()
+    {
+        return firstInt;
+    }
+
+    public Integer getSecondInt()
+    {
+        return secondInt;
+    }
 }
