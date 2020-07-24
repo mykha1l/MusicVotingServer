083923285275ac926cb72468af95a64566ce272c
hazletgathigia <hazletgathigia@gmail.com>
2020-07-24 16:22:10 +0200

    Added a method to get mp3 file list from song folder.

--- mvs-spring/src/main/java/com/haw/mvsspring/controller/HelloController.java
+++ mvs-spring/src/main/java/com/haw/mvsspring/controller/HelloController.java
@@ -2,7 +2,12 @@ package com.haw.mvsspring.controller;
 
 import com.haw.mvsspring.model.Adder;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @RequestMapping
 @RestController
@@ -23,12 +28,28 @@ public class HelloController {
 		return a + b;
 	}
 
-	@GetMapping("/subtraction")
+	@PostMapping("/subtraction")
 	public int getDifference(@RequestBody HashMap<String, Integer> myMap)
 	{
-		int a = myMap.get("firstInt");
-		int b = myMap.get("secondInt");
+		final int a = myMap.get("firstInt");
+		final int b = myMap.get("secondInt");
 
 		return a - b;
+	}
+
+
+	@GetMapping("/api/v1/songs")
+	public ArrayList<String> getSongList()
+	{
+		ArrayList<String> songList = new ArrayList<>();
+		File folder = new File("insert folder path" );	//what folder should we use? ..../src/resources/songFolder?
+		FileFilter fileFilter = new WildcardFileFilter("*.mp3");
+		File[] files = folder.listFiles(fileFilter);
+
+		for(int i = 0; i < files.length; i++)
+		{
+			songList.add(files[i].getName());
+		}
+		return songList;
 	}
 }
