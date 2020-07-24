671a114673a125f1a7df7632de035e564f52d09b
Mykhailo Svyrydovych <msvyrydovych@gmail.com>
2020-07-24 22:24:17 +0200

    Spring: add unit test for getSongList()

--- mvs-spring/src/test/java/com/haw/mvsspring/MvsSpringApplicationTests.java
+++ mvs-spring/src/test/java/com/haw/mvsspring/MvsSpringApplicationTests.java
@@ -1,13 +1,51 @@
 package com.haw.mvsspring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.haw.mvsspring.model.Song;
+import com.haw.mvsspring.model.SongList;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MvsSpringApplicationTests {
+	static File directory, song1, song2, song3;
+
+	@BeforeAll
+	public static void init() throws IOException {
+		directory = new File("../MVS-WebApp/songs/test/");
+		directory.mkdir();
+		song1 = new File("../MVS-WebApp/songs/test/song1.mp3");
+		song1.createNewFile();
+		song2 = new File("../MVS-WebApp/songs/test/song2.mp3");
+		song2.createNewFile();
+		song3 = new File("../MVS-WebApp/songs/test/song3.mp3");
+		song3.createNewFile();
+
+	}
 
 	@Test
-	void contextLoads() {
+	void readListOfSongs() throws IOException {
+
+		final ArrayList<Song> songsList = SongList.init("../MVS-WebApp/songs/test/");
+		assertEquals(songsList.get(0).getRawName(), "song1.mp3");
+		assertEquals(songsList.get(1).getRawName(), "song2.mp3");
+		assertEquals(songsList.get(2).getRawName(), "song3.mp3");
+	}
+
+	@AfterAll
+	public static void done() {
+		song1.delete();
+		song2.delete();
+		song3.delete();
+		directory.delete();
 	}
 
 }
