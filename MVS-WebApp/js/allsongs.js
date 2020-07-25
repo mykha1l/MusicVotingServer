c7c3bbd46d995b264b7b52d87ce3d398a1adc9d2
Mykhailo Svyrydovych <msvyrydovych@gmail.com>
2020-07-25 23:03:29 +0200

    Web-App: Fetch list of songs
    Fetch list of songs and display it in UI

--- MVS-WebApp/js/allsongs.js
+++ MVS-WebApp/js/allsongs.js
@@ -1,1 +1,23 @@
-alert("test");
+async function getAllSongs() {
+    const response = await fetch("http://localhost:8080/api/v1/songs");
+    const json = await response.json();
+
+    return json;
+}
+
+function createSongList(data) {
+    const songlist = document.getElementsByClassName("songlist")[0];
+    for (const song of data) {
+        const songNode = document.createElement("div");
+        songNode.className = "song";
+        songNode.innerHTML = song.rawName;
+        songlist.appendChild(songNode);
+    }
+}
+
+function init() {
+    getAllSongs().then(data => createSongList(data))
+        .catch(reason => console.log(reason.message));
+}
+
+document.onload = init();
