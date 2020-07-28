ccf029f7dae8469c8e17c26d8cd3313390b072d5
Parisa Ostadzadeh <parisa.ostadzadeh92@gmail.com>
2020-07-28 22:07:53 +0200

    Add CORS configuration
    To add posibility for request all song list from different browsers

--- mvs-spring/src/main/java/com/haw/mvsspring/MvsSpringApplication.java
+++ mvs-spring/src/main/java/com/haw/mvsspring/MvsSpringApplication.java
@@ -2,6 +2,9 @@ package com.haw.mvsspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MvsSpringApplication {
@@ -9,5 +12,15 @@ public class MvsSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MvsSpringApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/songs").allowedOrigins("*");
+            }
+        };
+    }
 
 }
