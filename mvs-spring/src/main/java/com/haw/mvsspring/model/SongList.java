14198022085dd50604f9e79e538fcd8ddb1b5644
Mykhailo Svyrydovych <msvyrydovych@gmail.com>
2020-08-20 22:18:30 +0200

    Spring:fix "pairs" should return always 2 songs
    When list is odd it should be made even by removing one song

--- mvs-spring/src/main/java/com/haw/mvsspring/model/SongList.java
+++ mvs-spring/src/main/java/com/haw/mvsspring/model/SongList.java
@@ -30,19 +30,31 @@ public class SongList {
     }
 
     public List<Song[]> getSongPairs() {
+        
+        makeSongListEven();
+        
+        return makePairsList();
+    }
+
+    private void makeSongListEven() {
+        if ((songList.size() % 2) != 0) {
+            songList.remove(songList.size() - 1);
+        }
+    }
+
+    private List<Song[]> makePairsList() {
+
         final List<Song[]> votingList = new ArrayList<>();
-        int i = 0;
-        while (i < songList.size()) {
+        for (int i = 0; i < songList.size(); i = i + 2) {
             final Song[] pair = new Song[2];
             pair[0] = songList.get(i);
-            if (++i < songList.size()) {
-                pair[1] = songList.get(i);
-            }
+            pair[1] = songList.get(i + 1);
             votingList.add(pair);
-            ++i;
         }
 
         return votingList;
     }
+
+    
 
 }
