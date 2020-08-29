10b8b6b823fc33b8cf7e2938a4ebd9a355fc8be3
Mykhailo Svyrydovych <msvyrydovych@gmail.com>
2020-08-29 11:37:53 +0200

    Spring: Remove url decoding
    To fix 'file not found' error

--- mvs-spring/src/main/java/com/haw/mvsspring/model/MyPlayer.java
+++ mvs-spring/src/main/java/com/haw/mvsspring/model/MyPlayer.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.List;
 
 import javazoom.jl.decoder.JavaLayerException;
@@ -14,7 +13,7 @@ public class MyPlayer {
 
     public static void play(List<String> filenames)
             throws FileNotFoundException, JavaLayerException, UnsupportedEncodingException {
-        final File file = new File("../MVS-WebApp/songs/" + URLEncoder.encode(filenames.get(0), "UTF-8" ));
+        final File file = new File("../MVS-WebApp/songs/" + filenames.get(0));
         final FileInputStream fis = new FileInputStream(file);
         final Player playMP3 = new Player(fis);
 
