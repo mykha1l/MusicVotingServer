cc3e4f315fdf031274b59d642e587c5df6938dd6
Mykhailo Svyrydovych <msvyrydovych@gmail.com>
2020-10-05 16:20:58 +0200

    Create multiple points in webpack config
    To generate multiple js files: for user page and for admin page

--- MVS-WebApp/mvs-webpack/webpack.config.js
+++ MVS-WebApp/mvs-webpack/webpack.config.js
@@ -2,10 +2,13 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/reactComponents/user.js',
+    entry: {
+        user: './src/js/reactComponents/user.js',
+        admin: './src/js/reactComponents/admin.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "user.js"
+        filename: "[name].js"
     },
     module: {
         rules: [
