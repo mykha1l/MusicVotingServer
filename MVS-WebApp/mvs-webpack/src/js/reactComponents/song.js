82ffe8695b3ff6a9cd6b82557022cdf4edf81c4c
Parisa Ostadzadeh <parisa.ostadzadeh92@gmail.com>
2020-10-15 18:06:29 +0200

    Quick fix frontend for displaying mostly voted songs

--- MVS-WebApp/mvs-webpack/src/js/reactComponents/song.js
+++ MVS-WebApp/mvs-webpack/src/js/reactComponents/song.js
@@ -4,15 +4,21 @@ export class Song extends React.Component {
 
     render() {
         if (this.props.data) {
-            return (
-                <div className='song' onClick={() => this.props.onClick(this.props.data.filename)}>
-                    <div className='artist'>{this.props.data.artist}</div>
-                    <div className='image'>
-                        <img width='200px' src={'data:image/png;base64,' + this.props.data.albumImage} />
+            if (!this.props.data.filename) {
+                return (
+                    <div className='song'>{this.props.data}</div>
+                )
+            } else {
+                return (
+                    <div className='song' onClick={() => this.props.onClick(this.props.data.filename)}>
+                        <div className='artist'>{this.props.data.artist}</div>
+                        <div className='image'>
+                            <img width='200px' src={'data:image/png;base64,' + this.props.data.albumImage} />
+                        </div>
+                        <div className='song-name'>{this.props.data.title}</div>
                     </div>
-                    <div className='song-name'>{this.props.data.title}</div>
-                </div>
-            )
+                )
+            }
         } else {
             return (
                 <div></div>
