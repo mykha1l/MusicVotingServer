af931445d8a28e018f990afe987c43e36423f778
Mykhailo Svyrydovych <msvyrydovych@gmail.com>
2020-12-12 22:30:03 +0100

    Move user related calls to separate controller

---
+++ mvs-spring/src/main/java/com/haw/mvsspring/controller/UsersController.java
@@ -0,0 +1,51 @@
+package com.haw.mvsspring.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.haw.mvsspring.authentication.SecurityConfig;
+import com.haw.mvsspring.model.User;
+import com.haw.mvsspring.model.UserDTO;
+import com.haw.mvsspring.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RequestMapping
+@RestController
+public class UsersController {
+
+    @Autowired
+    private SecurityConfig securityConfig;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/api/v1/user")
+    public String getUser() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+
+    @PostMapping("/api/v1/register")
+    RedirectView registerUser(@ModelAttribute final UserDTO userDTO, HttpServletRequest req) {
+        final User user = new User(userDTO.getUsername(),
+                securityConfig.passwordEncoder().encode(userDTO.getPassword()), "ROLE_USER", true);
+        userService.addUser(user);
+        userService.login(userDTO, req);
+        return new RedirectView("/index.html", false);
+    }
+
+    @PostMapping("/login")
+    RedirectView login(@ModelAttribute final UserDTO userDTO, HttpServletRequest req) {
+        userService.login(userDTO, req);
+        return new RedirectView("/index.html", false);
+    }
+
+}
--- mvs-spring/src/main/java/com/haw/mvsspring/service/UserService.java
+++ mvs-spring/src/main/java/com/haw/mvsspring/service/UserService.java
@@ -2,15 +2,28 @@ package com.haw.mvsspring.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import com.haw.mvsspring.dao.UserDao;
 import com.haw.mvsspring.model.User;
+import com.haw.mvsspring.model.UserDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    
+    @Autowired
+    private AuthenticationManager authManager;
 
     private final UserDao userDao;
 
@@ -26,6 +39,16 @@ public class UserService {
 
     public List<User> getUsers() {
         return userDao.getAllUsers();
+    }
+
+    public void login(UserDTO userDTO, HttpServletRequest req) {
+        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(userDTO.getUsername(),
+                userDTO.getPassword());
+        Authentication auth = authManager.authenticate(authReq);
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
+        HttpSession session = req.getSession(true);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, sc);
     }
 
 }
